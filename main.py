9da784a73c3c9fd0f6a333ae3847cad7779eec78
Fricodelco <rodion_anisimov@mail.ru>
2021-09-13 11:50:49 +0300

    add astar

--- main.py
+++ main.py
@@ -20,11 +20,16 @@ class Algorithm():
         coord = self.check_spawn_point()
         self.robot_coordinates.append(coord)
         self.order_iter = self.N+2
-        x_r = self.robot_coordinates[0][0]
-        y_r = self.robot_coordinates[0][1]
-        print(self.city_map[x_r][y_r])
-        for i in range(0, self.T):
-            self.get_new_orders_from_file(self.file)
+        # x_r = self.robot_coordinates[0][0]
+        # y_r = self.robot_coordinates[0][1]
+        # print(self.city_map[x_r][y_r])
+        self.get_new_orders_from_file(self.file)
+        print(self.robot_coordinates[0])
+        print(self.orders[0])
+        path = astar(self.city_map, self.robot_coordinates[0], (self.orders[0][0],self.orders[0][1]))
+        print(path)
+        # for i in range(0, self.T):
+        
             
             #analize orders and do the job
             # a = 1
@@ -40,11 +45,11 @@ class Algorithm():
             f_row = int(f[2])
             f_col = int(f[3])
             self.orders.append([s_row, s_col, f_row, f_col])
+            
     def get_new_orders_from_file(self, filename):
         with open(filename, 'r') as reader:
             data = reader.readlines()
             order_count = int(data[self.order_iter])
-            print(self.order_iter)
             for i in range(1, order_count+1):
                 coord = data[self.order_iter+i]
                 f = coord.split(" ")
@@ -102,6 +107,104 @@ class Algorithm():
         T = int(f[0])
         D = int(f[1])
         return N, MaxTips, Cost_c, city_map, T, D
+
+class Node():
+    """A node class for A* Pathfinding"""
+
+    def __init__(self, parent=None, position=None):
+        self.parent = parent
+        self.position = position
+
+        self.g = 0
+        self.h = 0
+        self.f = 0
+
+    def __eq__(self, other):
+        return self.position == other.position
+
+
+def astar(maze, start, end):
+    """Returns a list of tuples as a path from the given start to the given end in the given maze"""
+
+    # Create start and end node
+    start_node = Node(None, start)
+    start_node.g = start_node.h = start_node.f = 0
+    end_node = Node(None, end)
+    end_node.g = end_node.h = end_node.f = 0
+
+    # Initialize both open and closed list
+    open_list = []
+    closed_list = []
+
+    # Add the start node
+    open_list.append(start_node)
+
+    # Loop until you find the end
+    while len(open_list) > 0:
+
+        # Get the current node
+        current_node = open_list[0]
+        current_index = 0
+        for index, item in enumerate(open_list):
+            if item.f < current_node.f:
+                current_node = item
+                current_index = index
+
+        # Pop current off open list, add to closed list
+        open_list.pop(current_index)
+        closed_list.append(current_node)
+
+        # Found the goal
+        if current_node == end_node:
+            path = []
+            current = current_node
+            while current is not None:
+                path.append(current.position)
+                current = current.parent
+            return path[::-1] # Return reversed path
+
+        # Generate children
+        children = []
+        for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0)]: # Adjacent squares
+
+            # Get node position
+            node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
+
+            # Make sure within range
+            if node_position[0] > (len(maze) - 1) or node_position[0] < 0 or node_position[1] > (len(maze[len(maze)-1]) -1) or node_position[1] < 0:
+                continue
+
+            # Make sure walkable terrain
+            if maze[node_position[0]][node_position[1]] != 1:
+                continue
+
+            # Create new node
+            new_node = Node(current_node, node_position)
+
+            # Append
+            children.append(new_node)
+
+        # Loop through children
+        for child in children:
+
+            # Child is on the closed list
+            for closed_child in closed_list:
+                if child == closed_child:
+                    continue
+
+            # Create the f, g, and h values
+            child.g = current_node.g + 1
+            child.h = ((child.position[0] - end_node.position[0]) ** 2) + ((child.position[1] - end_node.position[1]) ** 2)
+            child.f = child.g + child.h
+
+            # Child is already in the open list
+            for open_node in open_list:
+                if child == open_node and child.g > open_node.g:
+                    continue
+
+            # Add the child to the open list
+            open_list.append(child)
+
 
 def main(args=None):
     alg = Algorithm()
